0e90f8c71f56be89d8a68603208b025a00cc6f1d
david karp <davidkarp94@gmail.com>
2022-10-31 17:08:04 -0300

    js added

--- js/app.js
+++ js/app.js
@@ -16,3 +16,48 @@ function agregarSalto(x) {
 
 agregarSalto(x);
 x.addListener(agregarSalto);
+
+// Tickets
+
+const estudiante = document.querySelector('.estudiante');
+const trainee = document.querySelector('.trainee');
+const junior = document.querySelector('.junior');
+
+const cantidad = document.getElementById('cantidad');
+var categoria = document.getElementById('categoria');
+
+const borrar = document.getElementById('borrar');
+const resumen = document.getElementById('resumen');
+const mostrarPago = document.getElementById('a-pagar');
+
+estudiante.addEventListener('click', function() {
+    categoria.value = 'estudiante';
+});
+
+trainee.addEventListener('click', function() {
+    categoria.value = 'trainee';
+});
+
+junior.addEventListener('click', function() {
+    categoria.value = 'junior';
+});
+
+resumen.addEventListener('click', function(e) {
+    e.preventDefault();
+    let cuenta;
+    if (categoria.value == 'estudiante') {
+        cuenta = 200 * 0.2 * cantidad.value;
+        mostrarPago.innerHTML = `$${cuenta}`;
+    } else if (categoria.value == 'trainee') {
+        cuenta = 200 * 0.5 * cantidad.value;
+        mostrarPago.innerHTML = `$${cuenta}`;
+    } else if (categoria.value == 'junior') {
+        cuenta = 200 * 0.85 * cantidad.value;
+        mostrarPago.innerHTML = `$${cuenta}`;
+    }
+})
+
+borrar.addEventListener('click', function(e) {
+    e.preventDefault();
+    mostrarPago.innerHTML = '';
+})
